b63f000d9dee9807388e38129eb429d007881dc1
Kamil Skubis <kskubis@gmail.com>
2022-06-15 14:41:21 +0200

    can switch between activities

--- app/src/main/java/skubis/kamil/notatnik/AddNoteActivity.java
+++ app/src/main/java/skubis/kamil/notatnik/AddNoteActivity.java
@@ -6,12 +6,14 @@ import android.os.Bundle;
 import android.widget.Button;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import skubis.kamil.notatnik.model.Note;
 import skubis.kamil.notatnik.persistence.NotepadDb;
 
 public class AddNoteActivity extends AppCompatActivity {
-    private TextInputEditText titleInputField,noteInput;
+    private TextInputLayout titleInput;
+    private TextInputEditText noteInput;
     private NotepadDb noteDatabase;
     private Note note;
 
@@ -21,8 +23,8 @@ public class AddNoteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_note_activity);
 
-        titleInputField = findViewById(R.id.titleInputField);
-        noteInput = findViewById(R.id.noteInput);
+        titleInput = (TextInputLayout) findViewById(R.id.titleInputField);
+        noteInput = (TextInputEditText) findViewById(R.id.noteInput);
         noteDatabase = NotepadDb.getInstance(AddNoteActivity.this);
         Button button = findViewById(R.id.button_save);
 
--- app/src/main/java/skubis/kamil/notatnik/MainActivity.java
+++ app/src/main/java/skubis/kamil/notatnik/MainActivity.java
@@ -2,14 +2,34 @@ package skubis.kamil.notatnik;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        FloatingActionButton floatingButton = (FloatingActionButton) findViewById(R.id.floatingButton);
+        floatingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchToAddNote();
+            }
+        });
+
+
+    }
+
+    private void switchToAddNote() {
+        Intent addNoteIntent = new Intent(this,AddNoteActivity.class);
+        startActivity(addNoteIntent);
     }
 
 }
